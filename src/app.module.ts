a8746a8405a11f49cb004bc15290515ff6afd853
liviafort <livia.fortunato@academico.ifpb.edu.br>
2022-12-16 15:36:20 -0300

    Conexão com o banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [CoursesModule],
+  //typeormodule serve para fazer conexão com o banco, deve inserir os parametros para a criação do database
+  imports: [CoursesModule, TypeOrmModule.forRoot({
+    type: 'postgres', //tipo de banco de dados
+    host: 'localhost', //rodando na própria maquina
+    port: 5432,
+    username: 'postgres', //usuarios default
+    password: 'sttp2022', 
+    database: 'postgres', //banco de dados padrão - serviço
+    autoLoadEntities: true, //carregamento automatico de entidades
+    synchronize: true, //vai criar automaticamente as tabelas no banco de dados
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
